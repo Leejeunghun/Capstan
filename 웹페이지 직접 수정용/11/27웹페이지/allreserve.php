4e73ba5366d7581ff7965c7627ad4fe4bb8126d9
Leejeunghun <201401662@inu.ac.kr>
2019-11-27 16:17:55 +0900

    update 1127

---
+++ 웹페이지 직접 수정용/11/27웹페이지/allreserve.php
@@ -0,0 +1,90 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
+<head>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="../css/main2.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+         <a href="./main2.php"><img  src="http://www.inu.ac.kr/user/ese/mycodyimages/top_logo.png" alt="ESE"height="80px">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="./time_storage.php">예약 신청 <span class="sr-only">(current)</span></a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="./allreserve.php"> 사용 현황 </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./reserve.php">나의 예약 현황</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./index_QR.php">QR코드 생성</a>
+      </li>
+          <form name="singOut" action="./signOut.php" method="ses_userid" onsubmit="return checkSubmit()">
+          <div class="line1" id="s3">
+              <input type="submit" value="로그아웃" class="submit" />
+          </div>
+          </form>
+    </ul>
+  </div>
+</nav>
+<style>
+  table {
+    width: 100%;
+    border: 1px solid #444444;
+  }
+  th, td {
+    border: 1px solid #444444;
+  }
+</style>
+ <?php
+
+        if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 10;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+        $conn=mysqli_connect("localhost","root","qwer123","esebird");
+        // Check connection
+        if (mysqli_connect_errno()){
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            die();
+        }
+
+        $total_pages_sql = "SELECT COUNT(*) FROM item";
+        $result = mysqli_query($conn,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+        $sql = "SELECT * FROM item LIMIT $offset, $no_of_records_per_page";
+  $res_data = mysqli_query($conn,$sql);
+  echo "<table border='2'>";
+  echo "<tr><td> 사용자 </td><td> 사용상태 </td><td>시간</td> </tr>";
+        while($row = mysqli_fetch_array($res_data)){
+                    echo "<tr><td> $row[User]</td><td> $row[Door_status]</td> <td>$row[time] </td> </tr>";
+        
+  }
+  echo "</table>";
+        mysqli_close($conn);
+    ?>
+    <ul class="pagination">
+        <li><a href="?pageno=1">First</a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
+            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+        </li>
+        <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+    </ul>
+</head>
+</html>
